d8d4164e5b0b7bb145d1ae03f9bfeb9d1131c3b4
Sahil-Bista <160755798+Sahil-Bista@users.noreply.github.com>
2025-02-16 17:24:38 +0545

    Refactor/Refactored the path for uploading images

--- server/services/fileUpload.js
+++ server/services/fileUpload.js
@@ -1,14 +1,7 @@
 import express from "express";
-import { fileURLToPath } from "url";
 import path from "path";
 
 export const uploadFileSetUpService = (app) => {
-  const __filename = fileURLToPath(import.meta.url);
-  //so it returns path like server/index.js
-  const __dirname = path.dirname(__filename);
-  // this returns only upto server and then it is attached to uploads
-  
-  app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-  //This basically helps in serving the file to the frontend
+  app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 };
 
